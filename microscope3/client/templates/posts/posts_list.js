e1b96ccc6054c786414785e32c381e107579b32c
Alexzarazua <alexzarazua99@gmail.com>
2017-05-02 12:36:57 +0200

    Pruebas clase

---
+++ microscope3/client/templates/posts/posts_list.js
@@ -0,0 +1,17 @@
+var postsImages = [
+  {
+    title: 'Camara de fotos',
+    url: 'https://lh6.googleusercontent.com/ytAFTnLCY9TcUNEbmedxJ8kknaiIOR0uQ78TK6z4S8wS5sywR2he7IFNAuZYuEXvD-OrONp51C2qSVY=w1319-h604'
+  },
+  {
+    title: 'KTM EXC 450cc ',
+    url: 'https://lh6.googleusercontent.com/bdiePZtjnll0L8YA4x5Vo0Gm-gvjTgfFtgVHdZvzPTDx1SXAis-b1lPjOKsXRyYhmMmgSJRXhywvj5E=w1319-h604'
+  },
+  {
+    title: 'Yamaha YZ 450cc',
+    url: 'https://lh3.googleusercontent.com/pwMlXnvF2ziIscVC4pI7PPtmFPVeLgY0LcOlSTbsGrgeKsHlmKAdQxbNuTRKbwL_aRQM9yUTudPPch0=w1319-h604'
+  }
+];
+Template.postsList.helpers({
+  posts: postsImages
+});
